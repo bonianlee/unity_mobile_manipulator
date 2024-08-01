80a7d522bc3e7db0bd58abb7334a66e50f8cab8b
ccasdqwe <109326892+bonianlee@users.noreply.github.com>
2024-08-01 17:49:37 +0800

    Add files via upload

--- ros_oculus/Assets/Scripts/HandPositionPublisher.cs
+++ ros_oculus/Assets/Scripts/HandPositionPublisher.cs
@@ -19,6 +19,8 @@ public class HandPositionPublisher : MonoBehaviour
     private float timeElapsed;
     private Vector3 position = Vector3.zero;
     private UnityEngine.Quaternion rotation = UnityEngine.Quaternion.identity;
+    Matrix3x3 R_he;
+    Vector3 tmp_rot_w_init;
 
     private RosHandPosMsg msg;
     // Start is called before the first frame update
@@ -26,6 +28,21 @@ public class HandPositionPublisher : MonoBehaviour
     {
         handController = GetComponent<HandController>();
         // platformState = GetComponent<PlatformMoving>();
+
+        // For 轉換手把旋轉軸
+        UnityEngine.Quaternion tmp_handle_quat = handController.GetHandRotation3DValue();
+        UnityEngine.Quaternion tmp_HMD_quat = handController.GetHMDRotation3DValue();
+        Matrix3x3 ground2handle_right_hand_rotm = Quat2Rotm_inv(tmp_handle_quat);
+        Matrix3x3 ground2HMD_right_hand_rotm = Quat2Rotm_inv(tmp_HMD_quat); // HMD 為右手系座標
+        Matrix3x3 R_gh = wholeBodyGround2handle_rotm(ground2handle_right_hand_rotm, ground2HMD_right_hand_rotm);
+        Matrix3x3 R_ge = new Matrix3x3(0, 0, 1, 1, 0, 0, 0, 1, 0);
+        Matrix3x3 R_hg = R_gh;
+        R_hg.Inverse();
+        R_he = R_ge * R_hg;
+        // Matrix3x3 R_ge_handle = R_he * R_gh;
+        // tmp_rot_w_init = rotm2angleAxis(R_ge_handle);
+
+
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<RosHandPosMsg>(topicname);
         msg = new RosHandPosMsg();
@@ -44,27 +61,37 @@ public class HandPositionPublisher : MonoBehaviour
             // HMD相對於大地座標
             Vector3 tmp_HMD_pos = handController.GetHMDPosition3DValue();
             UnityEngine.Quaternion tmp_HMD_quat = handController.GetHMDRotation3DValue();
+            
             // 旋轉向量(HMD到手把，再轉到機器人的基底座標 軸角表示)
-
             Matrix3x3 ground2handle_right_hand_rotm = Quat2Rotm_inv(tmp_handle_quat);
             Matrix3x3 ground2HMD_rotm = Quat2Rotm(tmp_HMD_quat); // HMD 為左手系座標
             Matrix3x3 ground2HMD_right_hand_rotm = Quat2Rotm_inv(tmp_HMD_quat); // HMD 為右手系座標
-            Vector3 tmp_rot = wholeBodyGround2handle_angleAxis(ground2handle_right_hand_rotm, ground2HMD_right_hand_rotm, rotation);
+            Matrix3x3 R_mh = m2handle_rotm(ground2handle_right_hand_rotm, ground2HMD_right_hand_rotm);
+            Matrix3x3 R_gh = wholeBodyGround2handle_rotm(ground2handle_right_hand_rotm, ground2HMD_right_hand_rotm);
+            // ----
+            Matrix3x3 R_ge_handle = R_he * R_gh;
+            Vector3 tmp_rot_w = rotm2angleAxis(R_ge_handle);
+            // ----
+            Vector3 tmp_rot_m = rotm2angleAxis(R_mh);
+
+            // Vector3 tmp_rot_w = rotm2angleAxis(R_gh);
             // 位置向量(HMD到手把，再旋轉到機器人基底座標)
             Vector3 tmp_posVec_HMD2handle = tmp_handle_pos - tmp_HMD_pos;
-            Vector3 tmp_pos = wholeBodyGround2handle_pos(tmp_posVec_HMD2handle, ground2HMD_rotm);
+            Vector3 tmp_pos_m = m2handle_pos(tmp_posVec_HMD2handle, ground2HMD_rotm);
+            Vector3 tmp_pos_w = wholeBodyGround2handle_pos(tmp_posVec_HMD2handle, ground2HMD_rotm);
 
             // 代入msg
-            SetHandPos(tmp_pos[0], tmp_pos[1], tmp_pos[2]);
-            SetHandRot(tmp_rot);
+            SetHandPos(tmp_pos_w, tmp_pos_m);
+            SetHandRot(tmp_rot_w, tmp_rot_m);
 
             // debug
             // Vector3 angleAxis_show = angle * axis;
             // Debug.Log("HMD:" + tmp_HMD_pos + "\n");
-            // Debug.Log("hand:" + tmp_handle_pos + "\n");
+            // Debug.Log("hand:" + tmp_rot_w + "\n");
+            
 
-            Debug.Log("tmp_pos:" + tmp_pos + "\n");
-            Debug.Log("tmp_rot:" + tmp_rot + "\n");
+            Debug.Log("tmp_pos:" + tmp_pos_w + "\n");
+            Debug.Log("tmp_rot:" + tmp_rot_w + "\n");
 
             // publish
             ros.Publish(topicname, msg);
@@ -72,19 +99,26 @@ public class HandPositionPublisher : MonoBehaviour
         }
     }
 
-    private void SetHandPos(float linear_x, float linear_y, float linear_z)
+    private void SetHandPos(Vector3 tmp_pos_w, Vector3 tmp_pos_m)
     {
-        // 左手系座標轉右手系座標，將 z 取反向
-        msg.hand_pos[0] = linear_x;
-        msg.hand_pos[1] = linear_y;
-        msg.hand_pos[2] = linear_z;
+        msg.hand_pos_w[0] = tmp_pos_w[0];
+        msg.hand_pos_w[1] = tmp_pos_w[1];
+        msg.hand_pos_w[2] = tmp_pos_w[2];
+
+        msg.hand_pos_m[0] = tmp_pos_m[0];
+        msg.hand_pos_m[1] = tmp_pos_m[1];
+        msg.hand_pos_m[2] = tmp_pos_m[2];
     }
 
-    private void SetHandRot(Vector3 tmp_rot)
+    private void SetHandRot(Vector3 tmp_rot_w, Vector3 tmp_rot_m)
     {
-        msg.hand_rot[0] = tmp_rot[0];
-        msg.hand_rot[1] = tmp_rot[1];
-        msg.hand_rot[2] = tmp_rot[2];
+        msg.hand_rot_w[0] = tmp_rot_w[0];
+        msg.hand_rot_w[1] = tmp_rot_w[1];
+        msg.hand_rot_w[2] = tmp_rot_w[2];
+
+        msg.hand_rot_m[0] = tmp_rot_m[0];
+        msg.hand_rot_m[1] = tmp_rot_m[1];
+        msg.hand_rot_m[2] = tmp_rot_m[2];
     }
 
     private Matrix3x3 Quat2Rotm(UnityEngine.Quaternion quat)
@@ -157,7 +191,50 @@ public class HandPositionPublisher : MonoBehaviour
         );
     }
 
-    private Vector3 wholeBodyGround2handle_angleAxis(Matrix3x3 ground2handle_right_hand_rotm, Matrix3x3 ground2HMD_right_hand_rotm, UnityEngine.Quaternion rotation)
+    private UnityEngine.Quaternion rotm2quaternion(Matrix3x3 rotm)
+    {
+        // 嘗試轉換手把的初始旋轉軸，使其與末端效應點的初始旋轉軸一致
+        UnityEngine.Quaternion quaternion_gh = new UnityEngine.Quaternion((float)(0.5 * Math.Sqrt(1 + rotm[1, 1] - rotm[2, 2] - rotm[3, 3])), (float)(0.5 * Math.Sqrt(1 - rotm[1, 1] + rotm[2, 2] - rotm[3, 3])), (float)(0.5 * Math.Sqrt(1 - rotm[1, 1] - rotm[2, 2] + rotm[3, 3])), (float)(0.5 * Math.Sqrt(1 + rotm[1, 1] + rotm[2, 2] + rotm[3, 3])));
+        if (Math.Sign(quaternion_gh[3]) * Math.Sign(rotm[3, 2] - rotm[2, 3]) < 0)
+            quaternion_gh[0] = -quaternion_gh[0];
+        if (Math.Sign(quaternion_gh[3]) * Math.Sign(rotm[1, 3] - rotm[3, 1]) < 0)
+            quaternion_gh[1] = -quaternion_gh[1];
+        if (Math.Sign(quaternion_gh[3]) * Math.Sign(rotm[2, 1] - rotm[1, 2]) < 0)
+            quaternion_gh[2] = -quaternion_gh[2];
+        return quaternion_gh;
+    }
+
+    private UnityEngine.Quaternion Calculate_adjustment_quat(Matrix3x3 curr_rotm)
+    {
+        Vector3 target_axis = new Vector3(0.5774f, 0.5774f, 0.5774f);
+
+        //
+        float trace = curr_rotm[1, 1] + curr_rotm[2, 2] + curr_rotm[3, 3];
+        float angle = (float)(Math.Acos((trace - 1) / 2));
+        Vector3 curr_axis = new Vector3();
+        curr_axis[0] = (float)((1 / (2 * Math.Sin(angle))) * (curr_rotm[3, 2] - curr_rotm[2, 3]));
+        curr_axis[1] = (float)((1 / (2 * Math.Sin(angle))) * (curr_rotm[1, 3] - curr_rotm[3, 1]));
+        curr_axis[2] = (float)((1 / (2 * Math.Sin(angle))) * (curr_rotm[2, 1] - curr_rotm[1, 2]));
+
+        //
+        UnityEngine.Quaternion currentRotation = UnityEngine.Quaternion.LookRotation(curr_axis);
+        UnityEngine.Quaternion desiredRotation = UnityEngine.Quaternion.LookRotation(target_axis);
+
+        return UnityEngine.Quaternion.Inverse(currentRotation) * desiredRotation;
+    }
+
+    private Matrix3x3 m2handle_rotm(Matrix3x3 ground2handle_right_hand_rotm, Matrix3x3 ground2HMD_right_hand_rotm)
+    {
+        // ground2handle_right_hand_rotm.Inverse();
+        ground2HMD_right_hand_rotm.Inverse();
+        Matrix3x3 HMD2ground_right_hand_rotm = ground2HMD_right_hand_rotm;
+        Matrix3x3 HMD2handle_rotm = ground2handle_right_hand_rotm * HMD2ground_right_hand_rotm; // the operator "*" definition please see its API
+        Matrix3x3 robotBase2HMD_rotm = new Matrix3x3(0, 0, -1, -1, 0, 0, 0, 1, 0);
+        Matrix3x3 robotBase2handle_rotm = HMD2handle_rotm * robotBase2HMD_rotm;
+        return robotBase2handle_rotm;
+    }
+    
+    private Matrix3x3 wholeBodyGround2handle_rotm(Matrix3x3 ground2handle_right_hand_rotm, Matrix3x3 ground2HMD_right_hand_rotm)
     {
         // ground2handle_right_hand_rotm.Inverse();
         ground2HMD_right_hand_rotm.Inverse();
@@ -167,14 +244,42 @@ public class HandPositionPublisher : MonoBehaviour
         Matrix3x3 robotBase2handle_rotm = HMD2handle_rotm * robotBase2HMD_rotm;
         double platform_yaw = Math.Atan2(2 * (rotation.w * rotation.z + rotation.x * rotation.y), 1 - 2 * (rotation.y * rotation.y + rotation.z * rotation.z));
         Matrix3x3 wholeBodyGround2robotBase_rotm = new Matrix3x3((float)Math.Cos(platform_yaw), (float)-Math.Sin(platform_yaw), 0, (float)Math.Sin(platform_yaw), (float)Math.Cos(platform_yaw), 0, 0, 0, 1);
-        Matrix3x3 wholeBodyGround2handle_rotm = robotBase2handle_rotm * wholeBodyGround2robotBase_rotm;
-        float trace = wholeBodyGround2handle_rotm[1, 1] + wholeBodyGround2handle_rotm[2, 2] + wholeBodyGround2handle_rotm[3, 3];
+        Matrix3x3 rotm = robotBase2handle_rotm * wholeBodyGround2robotBase_rotm;
+        return rotm;
+    }
+
+    private Vector3 rotm2angleAxis(Matrix3x3 rotm)
+    {
+        float trace = rotm[1, 1] + rotm[2, 2] + rotm[3, 3];
         float angle = (float)(Math.Acos((trace - 1) / 2));
         Vector3 axis = new Vector3();
-        axis[0] = (float)((1 / (2 * Math.Sin(angle))) * (wholeBodyGround2handle_rotm[3, 2] - wholeBodyGround2handle_rotm[2, 3]));
-        axis[1] = (float)((1 / (2 * Math.Sin(angle))) * (wholeBodyGround2handle_rotm[1, 3] - wholeBodyGround2handle_rotm[3, 1]));
-        axis[2] = (float)((1 / (2 * Math.Sin(angle))) * (wholeBodyGround2handle_rotm[2, 1] - wholeBodyGround2handle_rotm[1, 2]));
+        axis[0] = (float)((1 / (2 * Math.Sin(angle))) * (rotm[3, 2] - rotm[2, 3]));
+        axis[1] = (float)((1 / (2 * Math.Sin(angle))) * (rotm[1, 3] - rotm[3, 1]));
+        axis[2] = (float)((1 / (2 * Math.Sin(angle))) * (rotm[2, 1] - rotm[1, 2]));
         return angle * axis;
+    }
+
+    private Vector3 m2handle_pos(Vector3 tmp_posVec_HMD2handle, Matrix3x3 ground2HMD_rotm)
+    {
+        ground2HMD_rotm.Inverse();
+        Matrix3x3 HMD2ground_rotm = ground2HMD_rotm;
+        // 原本 HMD2handle 的向量是相對於大地座標，現在轉至相對於 HMD 座標系
+        Vector3 tmp_pos_HMD2handle = Vector3.zero;
+        tmp_pos_HMD2handle[0] = HMD2ground_rotm[1, 1] * tmp_posVec_HMD2handle[0] + HMD2ground_rotm[1, 2] * tmp_posVec_HMD2handle[1] + HMD2ground_rotm[1, 3] * tmp_posVec_HMD2handle[2];
+        tmp_pos_HMD2handle[1] = HMD2ground_rotm[2, 1] * tmp_posVec_HMD2handle[0] + HMD2ground_rotm[2, 2] * tmp_posVec_HMD2handle[1] + HMD2ground_rotm[2, 3] * tmp_posVec_HMD2handle[2];
+        tmp_pos_HMD2handle[2] = HMD2ground_rotm[3, 1] * tmp_posVec_HMD2handle[0] + HMD2ground_rotm[3, 2] * tmp_posVec_HMD2handle[1] + HMD2ground_rotm[3, 3] * tmp_posVec_HMD2handle[2];
+        // 將左手系座標轉至右手系座標
+        Vector3 tf2RightHandFrame = Vector3.zero;
+        for (int i = 0; i < 2; i++)
+            tf2RightHandFrame[i] = tmp_pos_HMD2handle[i];
+        tf2RightHandFrame[2] = -tmp_pos_HMD2handle[2];
+        // 將基底座標由 HMD 轉至機器人基底座標
+        Matrix3x3 robotBase2HMD_rotm = new Matrix3x3(0, 0, -1, -1, 0, 0, 0, 1, 0);
+        Vector3 tmp_result = Vector3.zero;
+        tmp_result[0] = robotBase2HMD_rotm[1, 1] * tf2RightHandFrame[0] + robotBase2HMD_rotm[1, 2] * tf2RightHandFrame[1] + robotBase2HMD_rotm[1, 3] * tf2RightHandFrame[2];
+        tmp_result[1] = robotBase2HMD_rotm[2, 1] * tf2RightHandFrame[0] + robotBase2HMD_rotm[2, 2] * tf2RightHandFrame[1] + robotBase2HMD_rotm[2, 3] * tf2RightHandFrame[2];
+        tmp_result[2] = robotBase2HMD_rotm[3, 1] * tf2RightHandFrame[0] + robotBase2HMD_rotm[3, 2] * tf2RightHandFrame[1] + robotBase2HMD_rotm[3, 3] * tf2RightHandFrame[2];
+        return tmp_result;
     }
 
     private Vector3 wholeBodyGround2handle_pos(Vector3 tmp_posVec_HMD2handle, Matrix3x3 ground2HMD_rotm)
